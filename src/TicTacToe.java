db710f2aebb41cbffcad5326d175ea4455e3fc55
Youssef Abali <50262857+Yousseff-Ab@users.noreply.github.com>
2020-03-15 17:49:45 +0100

    second commit

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,18 +6,18 @@ import javax.swing.*;
 public class TicTacToe {
 
     private static int[][] winCombinations = new int[][] {
-            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //horizontal wins
-            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, //vertical wins
-            {0, 4, 8}, {2, 4, 6}			 //diagonal wins
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //gagnes horizontal
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, //gagnes vertical
+            {0, 4, 8}, {2, 4, 6}			 //gagnes diagonal
     };
 
-    private static JButton buttons[] = new JButton[9]; //create 9 buttons
+    private static JButton buttons[] = new JButton[9]; //créer 9 boutons
 
 
 
     public static void main (String[] args)
     {
-        gamePanel(); //launch game
+        gamePanel(); //Lancer le jeu
     }
 
     private static void gamePanel(){
@@ -25,12 +25,12 @@ public class TicTacToe {
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 
 
-        JPanel panel = new JPanel(); //creating a panel with a box like a tic tac toe board
+        JPanel panel = new JPanel(); //créer un panel avec une boîte comme une planche de tic tac toe
         panel.setLayout (new GridLayout (3, 3));
         panel.setBorder (BorderFactory.createLineBorder (Color.gray, 5));
         panel.setBackground (Color.white);
 
-        for(int i=0; i<=8; i++){ //placing the button onto the board
+        for(int i=0; i<=8; i++){ //placer le bouton sur le tableau
             buttons[i] = new MyButton();
             panel.add(buttons[i]);
         }
@@ -38,19 +38,19 @@ public class TicTacToe {
         frame.getContentPane().add (panel);
         frame.pack();
         frame.setVisible(true);
-        frame.setSize(500, 500);// set frame size and let teh game begin
+        frame.setSize(500, 500);// définir la taille du frame et laisser le jeu commencer
     }
 
-    public static int xOrO=0; // used for counting
-    public static int[] var = {0,1};
+    public static int xOrO=0; // utilisé pour compter
+    public static int[] var = {0,1}; //utilisé pour varier le joueur qui débute
 
     private static class MyButton extends JButton
-            implements ActionListener {//creating own button class because JButton sucks:)
+            implements ActionListener {//création de sa propre classe de boutons
 
-        int again=1000;//set again at 1000 so we don't make the mistake we can play again
-        boolean win=false; // there is not a win
+        int again=1000;
+        boolean win=false; // il n'y a pas de gagne
         String letter; // x or o
-        public MyButton() {	// creating blank board
+        public MyButton() {	// création d'un tableau vierge
             super();
             letter=" ";
             setFont(new Font("Dialog", 1, 60));
@@ -66,12 +66,12 @@ public class TicTacToe {
                 letter="O";
                 xOrO=xOrO+1;
                 System.out.println(letter + "\n"+xOrO);
-            } // if user does click on a button that is already played, nothing will happen
+            } // si l'utilisateur clique sur un bouton déjà joué, rien ne se passera
 
-            setText(letter); // place the x or the o on the actual board
+            setText(letter); // placez le x ou le o
 
 
-            for(int i=0; i<=7; i++){ // check for the winning combinations
+            for(int i=0; i<=7; i++){ // vérifier les combinaisons gagnantes
                 if( buttons[winCombinations[i][0]].getText().equals(buttons[winCombinations[i][1]].getText()) &&
                         buttons[winCombinations[i][1]].getText().equals(buttons[winCombinations[i][2]].getText()) &&
                         buttons[winCombinations[i][0]].getText() != " "){//the winning is true
@@ -79,15 +79,15 @@ public class TicTacToe {
                 }
             }
 
-            if(win == true){ // if the game ends let the user know who wins and give option to play again
-                again=JOptionPane.showConfirmDialog(null, letter + " wins the game!  Do you want to play again?",letter + "won!",JOptionPane.YES_NO_OPTION);
+            if(win == true){ // si le jeu se termine, faites savoir à l'utilisateur qui gagne et donnez la possibilité de jouer à nouveau
+                again=JOptionPane.showConfirmDialog(null, letter + " gagne le match!  Voulez-vous rejouer?",letter + " gagne!",JOptionPane.YES_NO_OPTION);
 
-            } else if(xOrO == 9 && win == false){//tie game, announce and ask if the user want to play again
-                again=JOptionPane.showConfirmDialog(null, "The game was tie!  Do you want to play again?","Tie game!",JOptionPane.YES_NO_OPTION);
+            } else if(xOrO == 9 && win == false){//match nul, annoncer et demander si l'utilisateur souhaite rejouer
+                again=JOptionPane.showConfirmDialog(null, "Le match était nul!  Voulez-vous rejouer?","Match nul!",JOptionPane.YES_NO_OPTION);
                 win=true;
             }
 
-            if(again==JOptionPane.YES_OPTION && win==true){ // if the user want to play again clear all the button and start over
+            if(again==JOptionPane.YES_OPTION && win==true){ // si l'utilisateur veut rejouer effacer tout le bouton et recommencer avel le changement de rôle
                 clearButtons();
                 win=false;
                 if(var[0]==0 && var[1]==1) {
@@ -99,7 +99,7 @@ public class TicTacToe {
                 }
             }
             else if(again==JOptionPane.NO_OPTION){
-                System.exit(0); // exit game if the user do not want to play again
+                System.exit(0); // quitter le jeu si l'utilisateur ne veut pas rejouer
             }
         }
 
@@ -107,10 +107,10 @@ public class TicTacToe {
 
     public static void clearButtons(){
 
-        for(int i=0; i<=8; i++){// clear all 8 buttons
+        for(int i=0; i<=8; i++){// effacer tous les boutons
             buttons[i].setText(" ");
         }
-        xOrO=0; // reset the count
+        xOrO=0; // réinitialiser le compteur
 
     }
 
